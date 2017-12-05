1045a52b2e7472b10030b54046b06271483a00dd
Corentin Henry <corentinhenry@gmail.com>
2017-12-04 17:57:33 -0800

    hide domain and search behing accessors
    this provides a `get_search_or_domain` that is compatible with the glibc
    behavior where search and domain directives are mutually exclusive
    
    Also, using Option<String> and Option<Vec<String>> makes it clear
    whether a domain and search directive respectively were declared.

--- src/config.rs
+++ src/config.rs
@@ -1,4 +1,14 @@
-use {grammar, ScopedIp, Network, ParseError};
+use std::iter::{IntoIterator, Iterator};
+use std::slice::Iter;
+use {grammar, Network, ParseError, ScopedIp};
+
+#[derive(Copy, Clone, PartialEq, Eq, Debug)]
+enum LastSearch {
+    None,
+    Domain,
+    Search,
+}
+
 
 /// Represent a resolver configuration, as described in `man 5 resolv.conf` on linux.
 /// The options and defaults match those in this `man` page.
@@ -13,7 +23,7 @@ use {grammar, ScopedIp, Network, ParseError};
 ///     // Create a new config
 ///     let mut config = Config::new();
 ///     config.nameservers.push(ScopedIp::V4(Ipv4Addr::new(8, 8, 8, 8)));
-///     config.search.push("example.com".into());
+///     config.set_search(vec!["example.com".into()]);
 ///
 ///     // Parse a config
 ///     let parsed = Config::parse("nameserver 8.8.8.8\nsearch example.com").unwrap();
@@ -24,10 +34,15 @@ use {grammar, ScopedIp, Network, ParseError};
 pub struct Config {
     /// List of nameservers
     pub nameservers: Vec<ScopedIp>,
+    /// Indicated whether the last line that has been parsed is a "domain" directive or a "search"
+    /// directive. This is important for compatibility with glibc, since in glibc's implementation,
+    /// "search" and "domain" are mutually exclusive, and only the last directive is taken into
+    /// consideration.
+    last_search: LastSearch,
     /// Domain to append to name when it doesn't contain ndots
-    pub domain: Option<String>,
+    domain: Option<String>,
     /// List of suffixes to append to name when it doesn't contain ndots
-    pub search: Vec<String>,
+    search: Option<Vec<String>>,
     /// List of preferred addresses
     pub sortlist: Vec<Network>,
     /// Enable DNS resolve debugging
@@ -69,34 +84,32 @@ impl Config {
     /// # extern crate resolv_conf;
     /// use resolv_conf::Config;
     /// # fn main() {
-    /// assert_eq!(
-    ///     Config::new(),
-    ///     Config {
-    ///         nameservers: vec![],
-    ///         domain: None,
-    ///         search: vec![],
-    ///         sortlist: vec![],
-    ///         debug: false,
-    ///         ndots: 1,
-    ///         timeout: 5,
-    ///         attempts: 2,
-    ///         rotate: false,
-    ///         no_check_names: false,
-    ///         inet6: false,
-    ///         ip6_bytestring: false,
-    ///         ip6_dotint: false,
-    ///         edns0: false,
-    ///         single_request: false,
-    ///         single_request_reopen: false,
-    ///         no_tld_query: false,
-    ///         use_vc: false,
-    ///     });
+    /// let config = Config::new();
+    /// assert_eq!(config.nameservers, vec![]);
+    /// assert!(config.get_domain().is_none());
+    /// assert!(config.get_search().is_none());
+    /// assert_eq!(config.sortlist, vec![]);
+    /// assert_eq!(config.debug, false);
+    /// assert_eq!(config.ndots, 1);
+    /// assert_eq!(config.timeout, 5);
+    /// assert_eq!(config.attempts, 2);
+    /// assert_eq!(config.rotate, false);
+    /// assert_eq!(config.no_check_names, false);
+    /// assert_eq!(config.inet6, false);
+    /// assert_eq!(config.ip6_bytestring, false);
+    /// assert_eq!(config.ip6_dotint, false);
+    /// assert_eq!(config.edns0, false);
+    /// assert_eq!(config.single_request, false);
+    /// assert_eq!(config.single_request_reopen, false);
+    /// assert_eq!(config.no_tld_query, false);
+    /// assert_eq!(config.use_vc, false);
     /// # }
     pub fn new() -> Config {
         Config {
             nameservers: Vec::new(),
             domain: None,
-            search: Vec::new(),
+            search: None,
+            last_search: LastSearch::None,
             sortlist: Vec::new(),
             debug: false,
             ndots: 1,
@@ -136,5 +149,78 @@ impl Config {
     /// ```
     pub fn parse<T: AsRef<[u8]>>(buf: T) -> Result<Config, ParseError> {
         grammar::parse(buf.as_ref())
+    }
+
+    /// Return the suffixes declared in the last "domain" or "search" directive.
+    ///
+    /// ```rust
+    /// # extern crate resolv_conf;
+    /// use resolv_conf::{ScopedIp, Config};
+    /// # fn main() {
+    /// let config_str = "search example.com sub.example.com\ndomain localdomain";
+    /// let parsed_config = Config::parse(&config_str).expect("Failed to parse config");
+    /// let domains = parsed_config.get_last_search_or_domain()
+    ///                            .map(|domain| domain.clone())
+    ///                            .collect::<Vec<String>>();
+    /// assert_eq!(domains, vec![String::from("localdomain")]);
+    ///
+    /// let config_str = "domain localdomain\nsearch example.com sub.example.com";
+    /// let parsed_config = Config::parse(&config_str).expect("Failed to parse config");
+    /// let domains = parsed_config.get_last_search_or_domain()
+    ///                            .map(|domain| domain.clone())
+    ///                            .collect::<Vec<String>>();
+    /// assert_eq!(domains, vec![String::from("example.com"), String::from("sub.example.com")]);
+    /// # }
+    pub fn get_last_search_or_domain<'a>(&'a self) -> DomainIter<'a> {
+        match self.last_search {
+            LastSearch::Search => DomainIter::Search(
+                self.get_search()
+                    .and_then(|domains| Some(domains.into_iter())),
+            ),
+            LastSearch::Domain => DomainIter::Domain(self.get_domain()),
+            LastSearch::None => DomainIter::None,
+        }
+    }
+
+    /// Return the domain declared in the last "domain" directive.
+    pub fn get_domain(&self) -> Option<&String> {
+        self.domain.as_ref()
+    }
+
+    /// Return the domains declared in the last "search" directive.
+    pub fn get_search(&self) -> Option<&Vec<String>> {
+        self.search.as_ref()
+    }
+
+    /// Set the domain corresponding to the "domain" directive.
+    pub fn set_domain(&mut self, domain: String) {
+        self.domain = Some(domain);
+        self.last_search = LastSearch::Domain;
+    }
+
+    /// Set the domains corresponding the "search" directive.
+    pub fn set_search(&mut self, search: Vec<String>) {
+        self.search = Some(search);
+        self.last_search = LastSearch::Search;
+    }
+}
+
+/// A iterator returned by [`Config.get_last_search_or_domain`](struct.Config.html#method.get_last_search_or_domain)
+#[derive(Debug, Clone)]
+pub enum DomainIter<'a> {
+    Search(Option<Iter<'a, String>>),
+    Domain(Option<&'a String>),
+    None,
+}
+
+impl<'a> Iterator for DomainIter<'a> {
+    type Item = &'a String;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        match *self {
+            DomainIter::Search(Some(ref mut domains)) => domains.next(),
+            DomainIter::Domain(ref mut domain) => domain.take(),
+            _ => None,
+        }
     }
 }
