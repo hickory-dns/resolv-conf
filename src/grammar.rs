a9c79fff14f93c9b1325a1f4f9c762a9bafb0d86
brian-cook <brian.cook@gremlin.com>
2021-09-09 12:03:49 -0500

    rustc 1.55 emitted a warning that patches and features are mutually exclusive.  Inverting the the [no-]strict feature means that strict is the option and we no longer need to include a feature when building agent.

--- src/grammar.rs
+++ src/grammar.rs
@@ -29,7 +29,7 @@ quick_error!{
             display("option at line {} is not recognized", line)
         }
         /// Error returned when a invalid directive is found.
-        #[cfg(not(feature="no-strict"))]
+        #[cfg(feature="strict")]
         InvalidDirective(line: usize) {
             display("directive at line {} is not recognized", line)
         }
@@ -225,10 +225,10 @@ pub(crate) fn parse(bytes: &[u8]) -> Result<Config, ParseError> {
                     }
                 }
             }
-            #[cfg(not(feature="no-strict"))]
+            #[cfg(feature="strict")]
             _ => return Err(InvalidDirective(lineno)),
 
-            #[cfg(feature="no-strict")]
+            #[cfg(not(feature="strict"))]
             _ => /* ignore invalid directives when no-strict */ (),
 
         }
